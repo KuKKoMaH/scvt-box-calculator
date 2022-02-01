673350871952bf3acbdaaff5a339ccebb14ed55e
kirillzukov <kukkoman@gmail.com>
2022-02-01 23:10:13 +0300

    progress

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -1,8 +1,8 @@
-export const DATA_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQBIvxFrTBv2XFcu86VmJwIpcMpR0bDMzh7sbgJvKIQW8cNOwRK5gg3qH0DcjLJW4Ql5pRtQlRSbYrA/pub?gid=0&single=true&output=csv';
-export const CONST_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQBIvxFrTBv2XFcu86VmJwIpcMpR0bDMzh7sbgJvKIQW8cNOwRK5gg3qH0DcjLJW4Ql5pRtQlRSbYrA/pub?gid=1366746217&single=true&output=csv';
-export const MATERIALS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQBIvxFrTBv2XFcu86VmJwIpcMpR0bDMzh7sbgJvKIQW8cNOwRK5gg3qH0DcjLJW4Ql5pRtQlRSbYrA/pub?gid=2085623362&single=true&output=csv';
+export const DATA_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4SLAvJkfQ4sfiWiriwARvC5TDBcsxMHcmr3mG_aImuMGrI6c0KSUXRQJYqHY08ACQmdD9qMXovs8f/pub?gid=2103032520&single=true&output=csv';
+export const CONST_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4SLAvJkfQ4sfiWiriwARvC5TDBcsxMHcmr3mG_aImuMGrI6c0KSUXRQJYqHY08ACQmdD9qMXovs8f/pub?gid=1366746217&single=true&output=csv';
+export const MATERIALS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4SLAvJkfQ4sfiWiriwARvC5TDBcsxMHcmr3mG_aImuMGrI6c0KSUXRQJYqHY08ACQmdD9qMXovs8f/pub?gid=2085623362&single=true&output=csv';
 
-export const MEASURES = ['width', 'depth', 'height', 'cap', 'price', 'topPerList', 'bottomPerList'];
+export const MEASURES = ['width', 'depth', 'height', 'cap', 'topPerList', 'bottomPerList'];
 export const DEFAULT_FROM = 100;
 export const DEFAULT_TO = 500;
 
